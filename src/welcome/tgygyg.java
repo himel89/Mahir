308802147670378bbf3636f0bf50d97a2330804b
Himel <himel7152@gamil.com>
2022-02-16 14:33:07 -0500

    has been added

---
+++ src/welcome/tgygyg.java
@@ -0,0 +1,25 @@
+package welcome;
+
+public class tgygyg {
+  
+	int myVariable = 13;
+	
+	public static void main(String[] args) {
+	
+		byte myByte1, myByte2;
+		myByte1 = 127;
+		
+		short myShort = 1200;
+		 int myInt;
+		 long myLong = 123456789999999L;
+		 float myFloat = 3.14f;
+		 double Mydouble = 3.1212121212121;
+		 char myChar ='M';
+		 boolean myBoolean = true; 
+		 System.out.println();
+				
+		  
+
+	}
+
+}
